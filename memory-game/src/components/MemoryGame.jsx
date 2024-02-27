94ab6b7c4eb55a5a4edbb009d6c745ad0815766c
Quentout187 <quentin.caron@ecoles-epsi.net>
2024-02-27 11:53:31 +0100

    envoie du code

--- memory-game/src/components/MemoryGame.jsx
+++ memory-game/src/components/MemoryGame.jsx
@@ -3,13 +3,29 @@ import React, { useState } from 'react';
 import Board from './Board';
 
 const MemoryGame = () => {
-  const [cards, setCards] = useState([
-    { image: 'apple.jpg', flipped: false },
-    { image: 'poire.jpg', flipped: false },
-    { image: 'apple.jpg', flipped: false },
+  const [cards, setCards] = useState(() => {
+    const initialCards = [
+      { image: 'apple.jpg', flipped: false },
+      { image: 'poire.jpg', flipped: false },
+      { image: 'apple.jpg', flipped: false },
+      { image: 'poire.jpg', flipped: false },
+      { image: 'apple.jpg', flipped: false },
+      { image: 'poire.jpg', flipped: false },
+      { image: 'apple.jpg', flipped: false },
+    ];
 
+    const shuffledCards = shuffle(initialCards);
+    return shuffledCards;
+  });
 
-  ]);
+  function shuffle(array) {
+    const newArray = [...array];
+    for (let i = newArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+    }
+    return newArray;
+  }
 
   const handleCardClick = (index) => {
     const newCards = [...cards];
